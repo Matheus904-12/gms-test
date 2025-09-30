a65f23bf16229be87142f9015d2134d4003436f4
Matheus904-12 <matheuslucindo904@gmail.com>
2025-09-30 14:11:18 -0300

    ci-cd

---
+++ .history/cypress.config_20250930134612.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://127.0.0.1:8080',
+    video: true,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots'
+  },
+});
